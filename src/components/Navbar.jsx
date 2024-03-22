989edae6d03135775f5cc8f53dea32437a09eee2
Michél Kühne <3in6indun9@googlemail.com>
2024-03-22 11:28:05 +0100

    first initiation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='header-container-2'>
+        <div className='header-container'>
+            <div className='placeholder-header'>container für???</div>
+            <div className='navbar-container'>Navbar
+    
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
