6618950c48657dadbd5903e1a30fb8149cde9da7
Michél Kühne <3in6indun9@googlemail.com>
2024-03-22 14:58:36 +0100

    navCards, outsource, transition editing

---
+++ frontend/src/components/navbar/LeftNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LeftNav.css'
+
+function LeftNav() {
+  return (
+    <div className='glass-left-container'>
+      <div className='glass-left'>glass-container</div>
+    </div>
+  )
+}
+
+export default LeftNav
