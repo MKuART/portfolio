e146878fe63efb3a357f55c93cee3db391fd0686
Michél Kühne <3in6indun9@googlemail.com>
2024-03-30 17:45:15 +0100

    Gallery layout01

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -11,6 +11,7 @@ export const router = createBrowserRouter([
             {
                 path: '/gallery',
                 element: <Gallery />
+                
             },
             {
                 path: '/about',
