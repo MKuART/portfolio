58e8c5fb928101232b66a9b3f25315758f15c87b
Michél Kühne <3in6indun9@googlemail.com>
2024-04-03 16:26:08 +0200

    gallery connected with backend and mongoDB localhost:2222, one project present-view, error:picture from localhost with Data: Url-string

--- backend/models/projectModel.js
+++ backend/models/projectModel.js
@@ -3,7 +3,7 @@ import { Schema, model } from 'mongoose';
 const projectSchema = new Schema(
     {
         name: { type: String },
-        discription: { type: String },
+        description: { type: String },
         imageUrls: [String]
     },
     { strictQuery: true }
@@ -11,3 +11,65 @@ const projectSchema = new Schema(
 
 const ProjectModel = model('Project', projectSchema, 'projects');
 export default ProjectModel;
+
+
+/**
+ * [
+  {
+    "name": "MK-Ultra",
+    "description": "blender Animation",
+    "imageUrls": [
+      "../assets/mku/itteration.png",
+      "../assets/mku/itteration018.png"
+    ]
+  },
+  {
+    "name": "Retro Street Loop",
+    "description": "blender Animation for Music",
+    "imageUrls": [
+      "../assets/mku/itteration.png",
+      "../assets/mku/itteration018.png"
+    ]
+  },
+  {
+    "name": "Physiotherapie",
+    "description": "Design, Lorem ipsum quatschi la far taschi dalama",
+    "imageUrls": [
+      "../assets/schachKugelBackground01.png",
+      "../assets/schachKugelBackground01.png"
+    ]
+  },
+  {
+    "name": "Physiotherapie02",
+    "description": "Design, Lorem ipsum quatschi la far taschi dalama",
+    "imageUrls": [
+      "../assets/schachKugelBackground01.png",
+      "../assets/schachKugelBackground01.png"
+    ]
+  },
+  {
+    "name": "Physiotherapie03",
+    "description": "Design, Lorem ipsum quatschi la far taschi dalama",
+    "imageUrls": [
+      "../assets/schachKugelBackground01.png",
+      "../assets/schachKugelBackground01.png"
+    ]
+  },
+  {
+    "name": "Physiotherapie04",
+    "description": "Design, Lorem ipsum quatschi la far taschi dalama",
+    "imageUrls": [
+      "../assets/schachKugelBackground01.png",
+      "../assets/schachKugelBackground01.png"
+    ]
+  },
+  {
+    "name": "Physiotherapie05",
+    "description": "Design, Lorem ipsum quatschi la far taschi dalama",
+    "imageUrls": [
+      "../assets/schachKugelBackground01.png",
+      "../assets/schachKugelBackground01.png"
+    ]
+  }
+]
+ */
--- frontend/src/components/gallery/Gallery.jsx
+++ frontend/src/components/gallery/Gallery.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import './gallery.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
+import './gallery.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+const URI = "http://localhost:2222";
+const projectEndpoint = "/project";
+const relativePath = "../assets/mku/itteration.png";
+const absolutePath = "C:/Users/Michél/Desktop/portfolio/portfolio/frontend/src/" + relativePath.substring(2);
+
+// Jetzt können Sie den absoluten Pfad verwenden
+console.log(absolutePath);
 
 function Gallery() {
   const [startIndex, setStartIndex] = useState(0);
   const [selectedCardIndex, setSelectedCardIndex] = useState(null);
+  const [projects, setProjects] = useState([]);
 
-  const handleClick = (index) => {
-    setSelectedCardIndex(index);
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const fetchProjects = async () => {
+    try {
+      const response = await fetch(URI + projectEndpoint, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      });
+      if (!response.ok) {
+        console.error("Error fetching project data:");
+      } else {
+        const data = await response.json();
+        setProjects(data);  
+      }
+    } catch (error) {
+      console.error(`Error: ${error}`);
+    }
   };
-
-  const projects = [
-    { title: 'Title 1', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 2', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 3', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 4', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 5', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 6', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 7', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 8', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 9', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 10', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 11', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 12', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 13', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-    { title: 'Title 14', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aut accusantium unde facilis?' },
-
-  ];
-  const displayProjects = projects.slice(startIndex, startIndex + 6);
 
   const handleClickPrev = () => {
     if (startIndex > 0) {
@@ -40,6 +50,12 @@ function Gallery() {
     if (startIndex + 6 < projects.length) {
       setStartIndex(startIndex + 1);
     }
+  };
+
+  const displayProjects = projects.slice(startIndex, startIndex + 6);
+
+  const handleClick = (index) => {
+    setSelectedCardIndex(index);
   };
 
   return (
@@ -55,25 +71,27 @@ function Gallery() {
                 key={index}
                 onClick={() => setSelectedCardIndex(index)}
                 >
-                <div className='pic-container'></div>
-                <h3>{project.title}</h3>
+                <div className='pic-container'>
+                  {project.imageUrls[0] && (
+                    <img src={`file://${"C:/Users/Michél/Desktop/portfolio/portfolio/frontend/src/" + project.imageUrls[0].substring(2)}`} alt={`First Image`} />
+                  )}
+                </div>
+                <h3>{project.name}</h3>
                 <p className='pic-description'>{project.description}</p>
                 <div className='pic-description'></div>
               </div>
             ))}
           </div>
-          
-
           <div className='right-arrow' onClick={handleClickNext}>
-          <FontAwesomeIcon className='arrows-icon' icon={faAngleRight}/>
+            <FontAwesomeIcon className='arrows-icon' icon={faAngleRight}/>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
 
 /**
  * <div className='pics-container'>
